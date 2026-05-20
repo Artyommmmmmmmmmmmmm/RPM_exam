293e7d3615447f06fdbfc3d68fc40b4eac6c7993
Artyommmmmmmmmmmmmm <artyomk2006@mail.ru>
2026-05-21 10:15:25 +1100

    внесены правки

--- validator.py
+++ validator.py
@@ -2,13 +2,25 @@ from main import main
 
 def validate():
     text = ""
-    print(main(text))
+    a, b = main(text)
+    if not a:
+        print("Ошибка пустого текста успешно обработана")
+    else:
+        print("Ошибка пустого текста не обработана")
 
     text = "a" * 100001
-    print(main(text))
+    a, b = main(text)
+    if not a:
+        print("Ошибка слишком большого текста успешно обработана")
+    else:
+        print("Ошибка слишком большого текста не обработана")
 
     text = "🎉☕"
-    print(main(text))
+    a, b = main(text)
+    if not a:
+        print("Ошибка недопустимых символов в тексте успешно обработана")
+    else:
+        print("Ошибка недопустимых символов в тексте не обработана")
 
 if __name__ == "__main__":
     validate()
